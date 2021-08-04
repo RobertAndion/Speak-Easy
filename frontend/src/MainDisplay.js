cff306a19548007a961c38fb63b9cfa3cc723102
Robert <randion@usf.edu>
2021-08-03 21:58:15 -0400

    First commit with basic react app
    No customization yet

---
+++ frontend/src/MainDisplay.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function MainDisplay() {
+  return (
+    <div className="Application-Container">
+      <header className="App-header">
+Start of app
+      </header>
+    </div>
+  );
+}
+
+export default MainDisplay;
